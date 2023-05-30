cd8ef64818397fc80279cc4a5c1bb70e57028b0b
Ralph' Potato <FalconPilot@users.noreply.github.com>
2023-05-30 14:24:33 +0200

    Delete dist directory
    Remove dist

--- dist/constants.js
+++
@@ -1,3 +0,0 @@
-export const systemName = 'starclock';
-export const basePath = `systems/${systemName}`;
-export const distPath = `${basePath}/dist`;
--- dist/starclock.js
+++
@@ -1,2 +0,0 @@
-import { registerHooks } from './hooks.js';
-registerHooks();
